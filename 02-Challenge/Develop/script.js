a8c703f3c8a9a1674e6013986255f38374f748bb
sammyrey6 <121529677+sammyrey6@users.noreply.github.com>
2023-03-13 22:31:21 -0700

    *int

---
+++ 02-Challenge/Develop/script.js
@@ -0,0 +1,17 @@
+// Assignment Code
+//password generator
+  var generateBtn = document.querySelector("#generate");
+
+  // Write password to the #password input
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
+  }
+
+  // Add event listener to generate button
+  generateBtn.addEventListener("click", writePassword);
+//;
+  
